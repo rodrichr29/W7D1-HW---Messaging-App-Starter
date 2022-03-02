09d3dd9f93878eaa0b7516a13f8bb0f8f6c8f228
Chris <95760213+rodrichr29@users.noreply.github.com>
2022-03-02 00:01:33 -0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { Route, Routes, BrowserRouter as Router } from 'react-router-dom';
+import { Navbar, Nav, Container } from 'react-bootstrap';
+import Inbox from './Pages/Inbox';
+import Sent from './Pages/Sent';
+import Trash from './Pages/Trash';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return ( 
+        <Router>
+            <Navbar bg="light" expand="lg">
+                <Container>
+                    <Navbar.Brand href="#home">Messaging App Starter</Navbar.Brand>
+                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                    <Navbar.Collapse id="basic-navbar-nav">
+                        <Nav className="me-auto">
+                            <Nav.Link href="/">Inbox</Nav.Link>
+                            <Nav.Link href="/sent">Sent</Nav.Link>
+                            <Nav.Link href="/trash">Trash</Nav.Link>
+                        </Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
+            <Routes>
+                <Route path="/" exact component={Inbox} />
+                <Route path="/sent" component={Sent} />
+                <Route path="/trash" component={Trash} />
+            </Routes>
+        </Router>
+    );
 }
 
 export default App;
